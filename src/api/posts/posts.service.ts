12394afb03fec05a0a8a87f76208bbe3ec81fbb4
ndaemy <yuyaebean@gmail.com>
2023-06-16 23:48:04 +0900

    feat: Create posts routes

---
+++ src/api/posts/posts.service.ts
@@ -0,0 +1,42 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { customAlphabet } from 'nanoid';
+
+import { CreatePostDto } from './dto/create-post.dto';
+import { Post } from './entities/post.entity';
+
+@Injectable()
+export class PostsService {
+  posts: Post[] = [];
+  nanoid = customAlphabet('abcdefghijklmnopqrstuvwxyz1234567890', 6);
+
+  create(createPostDto: CreatePostDto) {
+    const post = {
+      id: this.nanoid(),
+      ...createPostDto,
+    };
+
+    this.posts.push(post);
+
+    return post;
+  }
+
+  findAll() {
+    return this.posts;
+  }
+
+  findOne(id: string) {
+    const post = this.posts.find(post => post.id === id);
+
+    if (!post) {
+      throw new NotFoundException('해당 아이디를 가진 포스트가 없습니다.');
+    }
+
+    return post;
+  }
+
+  remove(id: string) {
+    this.findOne(id);
+
+    this.posts = this.posts.filter(post => post.id !== id);
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 
 import { AuthModule } from '@/api/auth/auth.module';
+import { PostsModule } from '@/api/posts/posts.module';
 import { UsersModule } from '@/api/users/users.module';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, PostsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
