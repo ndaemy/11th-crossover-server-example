13ca85cbf644d2b599311799ba7368fb7962d608
ndaemy <yuyaebean@gmail.com>
2023-06-27 23:44:25 +0900

    fix: Update invalid id validation

--- src/api/auth/dto/register.dto.ts
+++ src/api/auth/dto/register.dto.ts
@@ -8,9 +8,9 @@ import {
 } from 'class-validator';
 
 export class RegisterDto {
-  @IsAlphanumeric('en-US', { message: '아이디는 영문, 숫자만 가능합니다.' })
-  @MinLength(5, { message: '아이디는 5자 이상이어야 합니다.' })
-  @MaxLength(10, { message: '아이디는 10자 이하여야 합니다.' })
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,10}$/, {
+    message: '아이디는 영문, 숫자를 포함한 5~10 자리여야 합니다.',
+  })
   id: string;
 
   @IsString()
